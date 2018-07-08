6e7b0aa3456d19aede8643427d929dd1444194a2
Juho Kaasalainen <juho@taiste.fi>
2018-07-09 00:48:42 +0300

    Added micro parser combinator lib

--- parsing.c
+++ parsing.c
@@ -2,17 +2,45 @@
 #include <stdlib.h>
 
 #include <editline/readline.h>
+#include "mpc.h"
 
 int main(int argc, char** argv) {
+
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+        " \
+        number : /-?[0-9]+/; \
+        operator : '+' | '-' | '*' | '/'; \
+        expr : <number> | '(' <operator> <expr>+ ')'; \
+        lispy : /^/ <operator> <expr>+ /$/; \
+        ",
+        Number, Operator, Expr, Lispy);
+
     puts("Lispy Version 0.1");
     puts("Press Ctrl+c to Exit\n");
 
     while(1) {
         char* input = readline("lispy> ");
         add_history(input);
-        printf("No you're a %s\n", input);
+
+        //printf("No you're a %s\n", input);
+        mpc_result_t result;
+        if(mpc_parse("<stdin>", input, Lispy, &result)) {
+            mpc_ast_print(result.output);
+            mpc_ast_delete(result.output);
+        } else {
+            mpc_err_print(result.error);
+            mpc_err_delete(result.error);
+        }
+
         free(input);
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
